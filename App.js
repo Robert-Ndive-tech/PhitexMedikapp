038fdd57a7d4f5d890cedbcc33fea700860c394b
robert <robertndive29@gmail.com>
2024-03-21 02:28:44 +0100

    Initial commit
    Generated by create-expo-app 2.1.1.

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Bug Ninza</Text>
+      <Image style={styles.image} source={require("./assets/BugNinza.png")} />
+
+      <Text style={styles.description}>Best Platform for programming</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
